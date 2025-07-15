9ba60cfed3c19b2571ff509c2999abed62217ded
MisterCage <mohammad.barbarji@icloud.com>
2025-07-16 00:15:30 +0400

    added scrambling effect after clicking images

--- src/fetchR&M.jsx
+++ src/fetchR&M.jsx
@@ -1,14 +1,30 @@
-import {useEffect } from 'react'
+import { useEffect } from "react"
 
-export default function FetchTable({url, characterList,setCharacterList}){
-    useEffect(()=>{fetch(url+"/[1,2]",{mode: "cors"}).then((data)=>data.json()).then((raw)=>setCharacterList(raw))
-    
-    return ()=> console.log("Cleanup")},[url,setCharacterList])
+export default function FetchTable({ url, characterList, setCharacterList }) {
+  useEffect(() => {
+    fetch(url + "/[1,2,3,4,5,6,7,9,10,11]", { mode: "cors" })
+      .then((data) => data.json())
+      .then((raw) => setCharacterList(raw))
 
-    const images = characterList.map((character)=> (
-        <div key={character.id}><img src = {character.image}/></div>
-    ))  
-    return (
-        <div>{images}</div>
-    )
+    return () => console.log("Cleanup")
+  }, [])
+
+  function scramble() {
+    let newArray = [...characterList]
+
+    for (let i = newArray.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1))
+      ;[newArray[i], newArray[j]] = [newArray[j], newArray[i]]
+    }
+
+    setCharacterList(newArray)
+  }
+
+  const images = characterList.map((character) => (
+    <div key={character.id}>
+      <img src={character.image} onClick={scramble} />
+      {character.name}
+    </div>
+  ))
+  return <div>{images}</div>
 }
